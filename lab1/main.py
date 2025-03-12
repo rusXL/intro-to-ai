4c98f4e58e74e1fd720023a4e948f991b4ba0f38
jusikXL <125464973+jusikXL@users.noreply.github.com>
2025-03-13 00:32:21 +0100

    feat: step-by-step visualisation

--- lab1/main.py
+++ lab1/main.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self, state, parent, action):
         self.state = state # let's start with coordinates (row, col)
@@ -21,8 +23,8 @@ class QueueFrontier: # let's start with BFS
         if self.empty():
             raise Exception("empty frontier")
         else:
-            node = self.frontier[0]
-            self.frontier = self.frontier[1:]
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
             return node
 
 class MazeSolver:
@@ -33,6 +35,9 @@ class MazeSolver:
         self.height = len(maze)
         self.width = len(maze[0])
         self.solution = None
+        self.step = 0  # Step counter for visualization
+        self.visited = set()
+        self.frontier = QueueFrontier()
 
     def neighbors(self, state):
         row, col = state
@@ -43,29 +48,36 @@ class MazeSolver:
             ("right", (row, col + 1))
         ]
 
+
+        # random.shuffle(candidates)
+
         result = []
         for action, (r, c) in candidates:
             if 0 <= r < self.height and 0 <= c < self.width and not self.walls[r][c]:
                 # if action is allowed i.e. we don't go out of maze or in the walls
                 result.append((action, (r, c)))
+
         return result
 
     def solve(self):
         # Initialize frontier to just the starting position
         start = Node(state=self.start, parent=None, action=None)
-        frontier = QueueFrontier()
-        frontier.add(start)
 
-        # Initialize an empty visited set
-        self.visited = set()
+        self.frontier.add(start)
+
 
         while True:
             # If nothing left in frontier, then no path
-            if frontier.empty():
+            if self.frontier.empty():
                 raise Exception("empty frontier")
 
             # Choose a node from the frontier
-            node = frontier.remove()
+            node = self.frontier.remove()
+            # Mark node as explored
+            self.visited.add(node.state)
+
+            self.output_image(f"step_{self.step}.png", current=node.state)
+            self.step += 1 # step is now 42
 
             # If node is the goal, then we have a solution
             if node.state == self.goal:
@@ -81,23 +93,19 @@ class MazeSolver:
                 self.solution = (actions, cells)
                 return
 
-            # Mark node as explored
-            self.visited.add(node.state)
-
             # Add neighbors to frontier
             for action, state in self.neighbors(node.state):
-                if not frontier.contains_state(state) and state not in self.visited:
+                if not self.frontier.contains_state(state) and state not in self.visited:
                     # if we haven't visited or are not to visit the node yet, add it to frontier
                     child = Node(state=state, parent=node, action=action)
-                    frontier.add(child)
+                    self.frontier.add(child)
 
-    def output_image(self, filename, show_solution=True, show_explored=False):
+    def output_image(self, filename, show_solution=True, show_explored=True, current=None):
         from PIL import Image, ImageDraw
 
         cell_size = 50
         cell_border = 2
 
-        # Create a blank canvas
         img = Image.new(
             "RGBA",
             (self.width * cell_size, self.height * cell_size),
@@ -108,32 +116,22 @@ class MazeSolver:
         solution = self.solution[1] if self.solution is not None else None
         for i, row in enumerate(self.walls):
             for j, col in enumerate(row):
-
-                # Walls
                 if col:
-                    fill = (40, 40, 40) # GRAY
-
-                # Start
+                    fill = (40, 40, 40)  # Wall (Gray)
                 elif (i, j) == self.start:
-                    fill = (255, 0, 0) # RED
-
-                # Goal
+                    fill = (255, 0, 0)  # Start (Red)
                 elif (i, j) == self.goal:
-                    fill = (0, 171, 28) # GREEN
-
-                # Solution
-                elif solution is not None and show_solution and (i, j) in solution:
-                    fill = (144, 238, 144) # LIGHT GREEN
-
-                # Explored
-                elif solution is not None and show_explored and (i, j) in self.visited:
-                    fill = (220, 235, 113) # YELLOW
-
-                # Empty cell
+                    fill = (0, 171, 28)  # Goal (Green)
+                elif solution and show_solution and (i, j) in solution:
+                    fill = (144, 238, 144)  # Solution Path (Light Green)
+                elif show_explored and (i, j) in self.visited:
+                    fill = (220, 235, 113)  # Explored (Yellow)
                 else:
-                    fill = (255, 255, 255) # WHITE
+                    fill = (255, 255, 255)  # Empty Cell (White)
 
-                # Draw cell
+                if current and (i, j) == current:
+                    fill = (0, 0, 255)  # Current Node (Blue)
+
                 draw.rectangle(
                     ([(j * cell_size + cell_border, i * cell_size + cell_border),
                       ((j + 1) * cell_size - cell_border, (i + 1) * cell_size - cell_border)]),
@@ -141,49 +139,6 @@ class MazeSolver:
                 )
 
         img.save(filename)
-
-def astar(maze, start, finish):
-    """
-    A* search
-
-    Parameters:
-    - maze: The 2D matrix that represents the maze with 0 represents emptry space and 1 represents a wall
-    - start: A tuple with the coordinates of starting position
-    - finish: A tuple with the coordinates of finishing position
-
-    Returns:
-    - Number of steps from start to finish, equals -1 if the path is not found
-    - Viz - everything required for step-by-step vizualization
-
-    """
-
-
-    solver = MazeSolver(maze, start, finish)
-    solver.solve()
-    solver.output_image("maze.png", show_explored=True)
-
-
-
-def vizualize(viz):
-    """
-    Vizualization function. Shows step by step the work of the search algorithm
-
-    Parameters:
-    - viz: everything required for step-by-step vizualization
-    """
-
-
-# Example usage:
-# grid = [
-#     [0, 1, 0, 0, 0],
-#     [0, 1, 0, 1, 0],
-#     [0, 0, 0, 1, 0],
-#     [1, 0, 1, 0, 0],
-#     [0, 0, 0, 1, 0]
-# ]
-#
-# start_position = (0, 0)
-# finish_position = (4, 4)
 
 grid = [
     [0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
@@ -201,15 +156,5 @@ grid = [
 start_position = (0, 0)
 finish_position = (9, 9)
 
-
-num_steps, viz = astar(grid, start_position, finish_position)
-
-# Print number of steps in path
-if num_steps != -1:
-    print(f"Path from {start_position} to {finish_position} using A* is {num_steps} steps.")
-
-else:
-    print(f"No path from {start_position} to {finish_position} exists.")
-
-# Vizualize algorithm step-by-step even if the path was not found
-vizualize(viz)
+solver = MazeSolver(grid, start_position, finish_position)
+solver.solve()
