db2af9f226cf4dafbcf26c737f93522e331cf267
jusikXL <125464973+jusikXL@users.noreply.github.com>
2025-03-28 22:54:49 +0100

    refactor(lab2): doc strings, code clarity

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -7,7 +7,7 @@ Action: TypeAlias = tuple[int, int]
 
 
 def actions(state: State) -> list[Action]:
-    """Legal moves for a state."""
+    """Legal moves for a `state`."""
     board, _ = state
     moves = []
     for i, sticks in enumerate(board):
@@ -17,7 +17,7 @@ def actions(state: State) -> list[Action]:
 
 
 def result(state: State, action: Action) -> State:
-    """Transition model. What is the result state of taking action from state."""
+    """Transition model. What is the result state of taking `action` from `state`."""
     pile, sticks = action
 
     new_board, new_turn = copy.deepcopy(state)
@@ -28,17 +28,15 @@ def result(state: State, action: Action) -> State:
 
 
 def terminal(state: State) -> bool:
-    """Terminal test. Determines when the game is over."""
+    """Decides whether the `state` is terminal. Determines when the game is over."""
     board, _ = state
     return sum(board) <= 1
 
 
 def utility(state: State) -> int:
-    """Final numerical value for the terminal state.
-
-    Returns:
-        -1: if the User won
-        +1: if the AI won
+    """Final numerical value for the terminal `state`.
+    -1: if User wins.
+    +1: if AI wins.
     """
     board, turn = state
 
@@ -51,7 +49,7 @@ def utility(state: State) -> int:
 
 
 def evaluate(state: State) -> int:
-    """Heuristic evaluation function for non-terminal states based on Nim-Sum."""
+    """Estimates expected utility of the game from the `state`. Heuristic evaluation based on Nim-Sum."""
     board, turn = state
     nim_sum = 0
 
@@ -65,15 +63,20 @@ def evaluate(state: State) -> int:
 
 
 def max_move(state: State, alpha: float, beta: float, depth: int) -> tuple[Action, int]:
+    """Behaviour of a player trying to maximize the score."""
     if terminal(state):
+        # return if reached the terminal state
         return None, utility(state)
     if depth == 0:
+        # return if reached the depth
         return None, evaluate(state)
 
-    action = None
-    value = float("-inf")
+    action = None  # best action
+    value = float("-inf")  # highest value
 
     for a in actions(state):
+        # find action `a` that produces the highest value
+        # of what min player can achieve if action is taken
         _, v = min_move(result(state, a), alpha, beta, depth - 1)
 
         if v > value:
@@ -89,15 +92,20 @@ def max_move(state: State, alpha: float, beta: float, depth: int) -> tuple[Actio
 
 
 def min_move(state: State, alpha: float, beta: float, depth: int) -> tuple[Action, int]:
+    """Behaviour of a player trying to minimize the score."""
     if terminal(state):
+        # return if reached the terminal state
         return None, utility(state)
     if depth == 0:
+        # return if reached the depth
         return None, evaluate(state)
 
-    action = None
-    value = float("inf")
+    action = None  # best action
+    value = float("inf")  # lowest value
 
     for a in actions(state):
+        # find action `a` that produces the lowest value
+        # of what max player can achieve if action is taken
         _, v = max_move(result(state, a), alpha, beta, depth - 1)
 
         if v < value:
@@ -115,29 +123,47 @@ def min_move(state: State, alpha: float, beta: float, depth: int) -> tuple[Actio
 # AI - max player: aims to maximize utility of the state
 # User - min player: aims to minimize utility of the state
 
-if __name__ == "__main__":
-    piles = []
-    print("Starting Nim!")
+
+# TODO: you are encouraged to refactor ⬇️⬇️⬇️
+# you can add better visualisation for the board
+# or improve input handling
+
+
+def init_game() -> State:
+    """Instructions to start the game."""
+    board = []
 
     num_piles = int(input("Input the number of piles: "))
 
     print("Input the number of sticks in each pile (separate with ENTER)")
     for _ in range(num_piles):
-        piles.append(int(input()))
+        board.append(int(input()))
 
-    state = (piles, 1)  # initial state
-    print("The person who removes the last stick loses!")
-    print("Example: to remove 3 sticks from pile 2, enter: 2 3")
+    print("Example Action: to remove 3 sticks from pile 2, enter: 2 3")
+
+    return (board, 1)  # initial state (board, turn)
+
+
+def end_game(state: State):
+    print(f"Board: {state[0]}")
+
+    if utility(state) == 1:
+        print("AI Wins! 🤖🏆")
+    else:
+        print("You Win! 👻🎉")
+
+
+if __name__ == "__main__":
+    DEPTH = 5
+    state = init_game()
 
     while not terminal(state):
-        board, turn = state  # Get current board & turn
+        board, turn = state
 
-        print("Current Pile State:", board)
+        print(f"Board: {board}")
 
         if turn % 2 == 0:  # AI's Turn
-            print("AI's turn ")
-            action, _ = max_move(state, float("-inf"), float("inf"), 5)
-
+            action, _ = max_move(state, float("-inf"), float("inf"), DEPTH)
             print(f"AI removes {action[1]} stick(s) from pile {action[0]}")
         else:  # User's Turn
             while True:
@@ -145,20 +171,17 @@ if __name__ == "__main__":
                     action = tuple(
                         map(
                             int,
-                            input("Your move (pile index sticks to remove): ").split(),
+                            input("Your move: ").split(),
                         )
                     )
                     if action in actions(state):
                         break
-                    print("Invalid move! Try again.")
+                    print("Try again")
                 except ValueError:
-                    print("Invalid input! Enter two numbers.")
+                    print("Try again")
 
-        state = result(state, action)  # Apply move
+        state = result(state, action)  # transition
+
+    end_game(state)
 
     # Game Over Message
-    print("\nFinal Pile State:", state[0])
-    if utility(state) == 1:
-        print("\nAI Wins! Better luck next time. 🤖🏆")
-    else:
-        print("\nYou Win! Congratulations! 🎉")
